c7b6971e1f9dad7d95f6ceebf0527f05b8a2eff8
Raymond Anthony Villaflores II <ravillaflores@student.apc.edu.ph>
2019-09-18 10:56:16 +0800

    Shift to Lab1

--- server.js
+++ server.js
@@ -12,28 +12,20 @@ var PORT = 3000;
 var express = require('express');
 
 // Create the server instance
-var exp_app = express();
+var app = express();
 
 // Print logs to the console and compress pages we send
-exp_app.use(express.logger());
-exp_app.use(express.compress());
+app.use(express.logger());
+app.use(express.compress());
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
-exp_app.use('/src', express.static(__dirname + '/src')); //Run Framework7 app
-exp_app.get('/', function (req, res) {
-
-	res.sendFile(path.join(__dirname + '/src/index.html')); 
-
-});
-
-var routes = require('./src/js/routes');  //RESTful APIs
-routes(exp_app);
+app.use(express.static(__dirname + '/static'));
 
 // Start the server
 var port = process.env.PORT || PORT; // 80 for web, 3000 for development
-exp_app.listen(port, function() {
+app.listen(port, function() {
 	console.log("Node.js server running on port %s", port);
 });
